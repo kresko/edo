a647a089b070d670c09b9b01bb29bfffaae9b279
kresko <kresko1995@gmail.com>
2025-05-21 06:55:07 +0200

    Added new css files, refactoring views

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,31 +1,33 @@
 <x-layout>
-    <h1>This is my home!!!!</h1>
+    <h1 class="width-1">Homepage</h1>
 
+    <!-- TODO: Implement cms logic -->
     <section class="content">
-        <img src="" alt="img">
+        <img src="https://picsum.photos/200/200" alt="img">
 
         <div class="content-text">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe soluta vero placeat deleniti alias excepturi exercitationem culpa, voluptatem deserunt dicta repudiandae totam quod consequuntur nulla, consequatur assumenda modi eius sequi?</p>
         </div>
     </section>
 
+    <!-- TODO: Implement cms logic -->
     <section class="content">
-        <img src="" alt="img">
+        <img src="https://picsum.photos/200/200" alt="img">
 
         <div class="content-text">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe soluta vero placeat deleniti alias excepturi exercitationem culpa, voluptatem deserunt dicta repudiandae totam quod consequuntur nulla, consequatur assumenda modi eius sequi?</p>
         </div>
     </section>
 
+    <!-- TODO: Implement cms logic -->
     <section class="content">
-        <img src="" alt="img">
+        <img src="https://picsum.photos/200/200" alt="img">
 
         <div class="content-text">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe soluta vero placeat deleniti alias excepturi exercitationem culpa, voluptatem deserunt dicta repudiandae totam quod consequuntur nulla, consequatur assumenda modi eius sequi?</p>
         </div>
     </section>
 
-    <p>test</p>
 </x-layout>
 
 
